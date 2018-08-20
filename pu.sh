5e7b8ec937fed0bab5e379d5aefb45d7386bd17a
Raymond Yang <raymond1415926@yahoo.com>
2018-08-20 11:44:44 +0800

    updating our push script so it updates the server as well.

--- pu.sh
+++ pu.sh
@@ -4,3 +4,9 @@ find . -type f -name '*.zzz' -delete
 git add .
 git commit .
 git push origin master
+
+## automagically updates the server side as well. Remember to add the SSH key
+## for this machine if you plan on doing this.
+ssh Vostok@ssh.pythonanywhere.com
+pull
+exit
